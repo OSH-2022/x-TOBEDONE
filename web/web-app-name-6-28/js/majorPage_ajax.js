ba2211d9351caaf76ed2c3fcd8a58b0da1e60b25
Sober_Clever <Sober_Clever@mail.ustc.edu.cn>
2022-06-28 22:50:25 +0800

    Modify WEB

--- web/web-app-name-6-28/js/majorPage_ajax.js
+++ web/web-app-name-6-28/js/majorPage_ajax.js
@@ -503,11 +503,14 @@ function fileDelete() {
 			form.append("isfolder", isfolder);
 			form.append("whose", whose);
 
-			//let ws2 = new WebSocket("ws://101.33.236.114:9090"); //创建WebSocket连接
+			let ws2 = new WebSocket("ws://101.33.236.114:9090"); //创建WebSocket连接
 			
-			//if(isfolder == 0) //网页端的删除文件行为需要同步到图数据库上
-			//	w2.send("('delete', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + whose + "'})");
-
+			if(isfolder == 0) //网页端的删除文件行为需要同步到图数据库上
+				ws2.onopen = function()
+				{
+					w2.send("('delete', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + whose + "'})");
+				}
+				
 			$.ajax({
 				url:"FileDownloader!deleteRegister.action",
 				type:"POST",
@@ -649,11 +652,13 @@ function fileRename() {
 			form.append("whose", $.cookie("username"));
 
 			// 网页端的重命名行为需要同步到图数据库上
-			//let ws2 = new WebSocket("ws://101.33.236.114:9090"); //创建WebSocket连接
+			let ws2 = new WebSocket("ws://101.33.236.114:9090"); //创建WebSocket连接
 			
-			//ws2.send("('rename', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + $.cookie("username") + "', 'newname': '" + new_name + "'})");
-			//console.log("('rename', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + $.cookie("username") + "', 'newname': '" + new_name + "'})");
-			
+			ws2.onopen = function()
+			{
+				ws2.send("('rename', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + $.cookie("username") + "', 'newname': '" + new_name + "'})");
+				console.log("('rename', {'name': '" + name + "', 'path': '" + path + "', 'owner': '" + $.cookie("username") + "', 'newname': '" + new_name + "'})");
+			}
 
 			console.log(path + " " + name + " " + new_name);
 
