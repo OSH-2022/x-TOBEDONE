77cf78bada8bfeef3a50f1f3a9f5ef625d2b4174
Kimagure <re2477036@mail.ustc.edu.cn>
2023-05-16 14:37:46 +0800

    删除了多余的前端文件夹，精简项目

--- web/grandtest/jsp/mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
-	$charset = 'UTF-8';
-?>
